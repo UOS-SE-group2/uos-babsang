91b2a8a26ed9c99c93f68be34458b225b3fd3d92
tank3a <tank3a@gmail.com>
2021-10-11 22:06:12 +0900

    Installation 2

--- src/server.js
+++ src/server.js
@@ -1,5 +1,17 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
+const PORT = 5000;
 
-console.log("Hello2");
+const logger = morgan("dev");
+
+app.set("views", process.cwd()+"/src/views");
+app.set("view engine", "pug");
+app.set("x-powered-by", "false");
+app.use(logger);
+
+const handleListening = () => {
+    console.log("Server Listening");
+}
+app.listen(PORT, handleListening);

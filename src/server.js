7cf2758163ebd14021ef07d2284f5085dab9f141
jyj1111 <89414343+jyj1111@users.noreply.github.com>
2021-11-30 01:28:22 +0900

    session, cookie parser 등등 수정

--- src/server.js
+++ src/server.js
@@ -3,12 +3,27 @@ import morgan from "morgan";
 import rootRoute from "./server/pages/rootRoute";
 import customerRoute from "./server/pages/customerRoute";
 import managerRoute from "./server/pages/managerRoute";
+const bodyParser = require('body-parser');
+const session = require('express-session')
+const compression = require('compression');
+const FileStore = require('session-file-store')(session);
+const cookieParser = require('cookie-parser')
 
 const app = express();
 const PORT = 5000;
 
 const logger = morgan("dev");
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(compression());
+app.use(cookieParser())
 
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    // session 추가되면 session 폴더 안에 파일로 저장
+    store:new FileStore()
+  }))
 app.set("views", process.cwd()+"/src/views");
 app.set("view engine", "pug");
 app.set("x-powered-by", "false");

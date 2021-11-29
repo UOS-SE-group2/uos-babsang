f347996dcc81a13022524c866ad21d8e4fe76b01
jyj1111 <89414343+jyj1111@users.noreply.github.com>
2021-11-30 01:18:45 +0900

    db.js파일 수정

--- src/db.js
+++ src/db.js
@@ -1,31 +1,12 @@
-import mysql from "mysql2";
-const pool = mysql.createPool({ 
+const mysql=require('mysql');
+
+const db = mysql.createConnection({ 
     host:'54.180.16.45', 
-    user:'steve', 
+    user:'jyj', 
     password:'password', 
     port:'3306', 
-    database:'stevedb' 
+    database:'jyjdb' 
 });
-module.exports = {
-    pool: pool
-};
-/*let connection = mysql.createConnection(pool); // DB 커넥션 생성
-connection.connect();   // DB 접속
-let testQuery = "INSERT INTO `category` (`category_name`,`category_image`) VALUES ('양식','http://www.kdf.co.kr');";
-connection.query(testQuery, function (err, results, fields) { // testQuery 실행
-    if (err) {
-        console.log(err);
-    }
-    console.log(results);
-});
-connection.end(); // DB 접속 종료
 
-
-
-/*app.get('/db-connection', (req, res)=>{
-    var sql = 'SELECT * FROM GROUPLIST WHERE GROUP_NAME = ?'
-    mysqlDB.query(sql, '졸프', function (err, results) {
-        if (err) console.log(err);
-        else res.send(results);
-      });
-})*/
+db.connect();
+module.exports=db;

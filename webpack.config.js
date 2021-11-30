bac79db5fa83d8d8210e4aec842fd92a3d5ad4ab
tank3a <tank3a@gmail.com>
2021-11-30 12:37:47 +0900

    Setup for client-part(css)

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,18 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
-    
-}
+    entry: {
+        main: "./src/client/js/main.js"
+    },
+    plugins: [new MiniCssExtractPlugin({
+        filename: "css/styles.css",
+    })],
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+      ],
+    },
+  };

1b1bdacf1fe77aa04fecfedd488ad6a9d955842e
tank3a <tank3a@gmail.com>
2021-12-04 14:45:41 +0900

    프론트 js 동작하도록 변경

--- src/server/pages/rootRoute.js
+++ src/server/pages/rootRoute.js
@@ -1,16 +1,17 @@
 import express from "express";
-import {home, getLogin, postLogin, search, restaurant, getCatagory} from "../controllers/homeController";
+import {home, getLogin, search, restaurant, getCategory, logout} from "../controllers/homeController";
 import { getJoinAsCustomer, postJoinAsCustomer } from "../controllers/customerController";
 import { getJoinAsManager, postJoinAsManager } from "../controllers/managerController";
 
 const rootRoute = express.Router();
 
 rootRoute.get("/", home);
-rootRoute.get("/catagory/:id(^[1-5]$)", getCatagory);
-rootRoute.route("/login").get(getLogin).post(postLogin);
+rootRoute.get("/category/:id([1-5])", getCategory);
+rootRoute.route("/login").get(getLogin);
 rootRoute.route("/join_as_customer").get(getJoinAsCustomer).post(postJoinAsCustomer);
 //rootRoute.route("/join_as_manager").get(getJoinAsManager).post(postJoinAsManager);
 rootRoute.get("/search", search);
 rootRoute.get("/restaurant/:id", restaurant);
+rootRoute.get("/logout", logout);
 
 export default rootRoute;
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,36 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
 
+const BASE = "./src/client/js/";
 module.exports = {
-    entry: {
-        main: "./src/client/js/main.js"
-    },
-    plugins: [new MiniCssExtractPlugin({
-        filename: "css/styles.css",
-    })],
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: [MiniCssExtractPlugin.loader, "css-loader"],
-        },
-      ],
-    },
-  };
+  entry: {
+      main: BASE + "main.js",
+//      home: BASE + "home.js"
+  },
+  plugins: [new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+  })],
+  output: {
+    filename: "js/[name].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true
+  },
+  mode:"development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [['@babel/preset-env', {targets:"defaults"}]]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+};

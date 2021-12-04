9ed40e89ada75d9336a5d1fae0682346c4d857fd
jyj1111 <89414343+jyj1111@users.noreply.github.com>
2021-12-04 11:34:40 +0900

    검색 컨트롤러 작성

--- src/server/controllers/homeController.js
+++ src/server/controllers/homeController.js
@@ -39,7 +39,27 @@ export const postLogin = (req, res) => {
 }
 
 //검색된 화면
-export const search = (req, res) => res.render("search");
+export const search = (req, res) => {
+    const {restaurantName}=req.body;
+    console.log(restaurantName);
+    if (restaurantName) {
+
+        db.query('SELECT * FROM restaurant WHERE restaurantName= ?', [restaurantName], function(error, results, fields) {
+            if (error) throw error;
+            if (results.length > 0) {
+                const restaurantId=results[0].restaurantId;
+                console.log(restaurantId);
+                res.render("restaurant",{restaurantId});
+            } else {         
+                res.status(400).send('<script type="text/javascript">alert("검색 정보가 없습니다."); document.location.href="/";</script>');    
+            }            
+        });
+    } else {        
+        res.send('<script type="text/javascript">alert("검색어를 입력하세요!"); document.location.href="/";</script>');    
+        res.redirect("/");
+    }
+    
+}
 
 //특정 가게 화면
 export const restaurant = (req, res) => res.render("restaurant");

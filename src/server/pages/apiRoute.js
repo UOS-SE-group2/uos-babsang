281c42a42c5fd269fb8deea1d12f760ad8a39a76
jyj1111 <89414343+jyj1111@users.noreply.github.com>
2021-12-06 00:04:12 +0900

    apiroute  추가

--- src/server/pages/apiRoute.js
+++ src/server/pages/apiRoute.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { orderConfirm, orderDenied, deleteMenu } from "../controllers/managerController";
 import { deleteReview } from "../controllers/customerController";
-
+import { basket,order } from "../controllers/customerController";
 const apiRoute = express.Router();
 
 apiRoute.post("/manager/orderlist/:id/confirmed", orderConfirm);
 apiRoute.post("/manager/orderlist/:id/denied", orderDenied);
 apiRoute.get("/customer/myreviews/:reviewId", deleteReview);
 apiRoute.get("/api/manager/addmenus/:menuId/delete", deleteMenu);
-
+apiRoute.get("/restaurant/:id/:menuId",basket);
+apiRoute.get("/order/:id",order);
 export default apiRoute;

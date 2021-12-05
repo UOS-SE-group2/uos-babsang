56c9e4fd06924bbe6b32a6769d4cbc5ae6e68ef3
tank3a <tank3a@gmail.com>
2021-12-05 20:42:30 +0900

    join 페이지 css

--- src/server/pages/rootRoute.js
+++ src/server/pages/rootRoute.js
@@ -4,7 +4,7 @@ import {home, getLogin, search, restaurant, getCategory, logout, getJoin} from "
 const rootRoute = express.Router();
 
 rootRoute.get("/", home);
-rootRoute.get("/category/:id([1-5])", getCategory);
+rootRoute.get("/category/:id([1-6])", getCategory);
 rootRoute.get("/login", getLogin);
 rootRoute.get("/join", getJoin);
 rootRoute.get("/logout", logout);
